451d622f8769aea46d54d97aa882b096d0ec2f8e
youssefezz12 <youssefezz@gmail.com>
2026-05-21 16:36:05 +0300

    Fix buffer initialization bug causing crash

--- source-code-debugging/main.cpp
+++ source-code-debugging/main.cpp
@@ -2,26 +2,28 @@
 #include <cstdlib>
 #include <cstring>
 
-char* buf;
+char buf[50];
 
 int sum_to_n(int num) {
-  int i, sum = 0;
-  for (i = 1; i <= num; i++) sum += i;
-  return sum;
+    int i, sum = 0;
+    for (i = 1; i <= num; i++)
+        sum += i;
+    return sum;
 }
 
 void printSum() {
-  char line[10];
-  printf("Enter a number: ");
-  fgets(line, 10, stdin);
+    char line[10];
+    printf("Enter a number: ");
 
-  if (line != NULL) strtok(line, "\n");
+    if (fgets(line, sizeof(line), stdin) != NULL) {
+        strtok(line, "\n");
 
-  sprintf(buf, "sum=%d", sum_to_n(atoi(line)));
-  printf("%s\n", buf);
+        sprintf(buf, "sum=%d", sum_to_n(atoi(line)));
+        printf("%s\n", buf);
+    }
 }
 
 int main() {
-  printSum();
-  return 0;
+    printSum();
+    return 0;
 }
